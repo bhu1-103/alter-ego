1143dbf7313b9005922f9b7759ffbfed919855ce
bhu1-103 <103.bhuvan@gmail.com>
2025-01-30 02:51:19 +0530

    code cleaning

--- scripts/ask-me-anything.sh
+++ scripts/ask-me-anything.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/zsh
 
-touch ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
-touch ~/dev/logs/alter-ego/$(date +%y-%m-%d)-responses.txt
+logs_dir="/home/bhu2/dev/logs/alter-ego"
+
+touch logs_dir/$(date +%y-%m-%d)-prompts.txt
+touch logs_dir/$(date +%y-%m-%d)-responses.txt
 
 model=$(ollama list | tail -n +2 | awk -F " " '{print $1}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "")
 
 prompt=$(rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "Jarvis with $model here")
-#echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
 
 if [ -n "$prompt" ]; then
-	echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
+	echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> logs_dir/$(date +%y-%m-%d)-prompts.txt
 	curl http://localhost:11434/api/generate -d '{"model": "'$model'",
 		"prompt":"1 line response please: '"$prompt"'",
 		"stream":false
 		}' > ~/dev/cache/response.json
-	jq ".response" ~/dev/cache/response.json >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-responses.txt
+	jq ".response" ~/dev/cache/response.json >> logs_dir/$(date +%y-%m-%d)-responses.txt
+	notify-send "alter-ego_says" "$(tail -n 1 logs_dir/25-01-30-responses.txt)"
 else
 	echo "nani sore.."
 fi
