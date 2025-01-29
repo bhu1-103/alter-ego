bfcff7e4ff96bd89fdc50a0cc3cefa7424382754
bhu1-103 <103.bhuvan@gmail.com>
2025-01-30 03:13:36 +0530

    added recent chats

--- scripts/ask-me-anything.sh
+++ scripts/ask-me-anything.sh
@@ -2,21 +2,21 @@
 
 logs_dir="/home/bhu2/dev/logs/alter-ego"
 
-touch $logs_dir/$(date +%y-%m-%d)-prompts.txt
-touch $logs_dir/$(date +%y-%m-%d)-responses.txt
+touch $logs_dir/prompts.txt
+touch $logs_dir/responses.txt
 
 model=$(ollama list | tail -n +2 | awk -F " " '{print $1}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "")
 
-prompt=$(rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "Jarvis with $model here")
+prompt=$(tail -n 5 $logs_dir/prompts.txt | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "Jarvis with $model here")
 
 if [ -n "$prompt" ]; then
-	echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> $logs_dir/$(date +%y-%m-%d)-prompts.txt
+	echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> $logs_dir/prompts.txt
 	curl http://localhost:11434/api/generate -d '{"model": "'$model'",
 		"prompt":"1 line response please: '"$prompt"'",
 		"stream":false
 		}' > ~/dev/cache/response.json
-	jq ".response" ~/dev/cache/response.json >> $logs_dir/$(date +%y-%m-%d)-responses.txt
-	notify-send "alter-ego_says" "$(tail -n 1 $logs_dir/$(date +%y-%m-%d)-responses.txt)"
+	jq ".response" ~/dev/cache/response.json >> $logs_dir/responses.txt
+	notify-send "alter-ego_says" "$(tail -n 1 $logs_dir/responses.txt)"
 else
 	echo "nani sore.."
 fi
