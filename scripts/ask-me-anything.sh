b5c3e09b8f219588635565e007469d6dd8ddf0fd
bhu1-103 <103.bhuvan@gmail.com>
2025-01-30 02:38:52 +0530

    fixed empty input being registered

--- scripts/ask-me-anything.sh
+++ scripts/ask-me-anything.sh
@@ -3,12 +3,13 @@
 touch ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
 touch ~/dev/logs/alter-ego/$(date +%y-%m-%d)-responses.txt
 
-model=$(ollama list | tail -n +2 | awk -F " " '{print $1}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-7 -p "")
+model=$(ollama list | tail -n +2 | awk -F " " '{print $1}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "")
 
-prompt=$(rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-7 -p "Jarvis with $model here")
-echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
+prompt=$(rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-3 -p "Jarvis with $model here")
+#echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
 
 if [ -n "$prompt" ]; then
+	echo "$(date +%H:%M) | Model: $(printf "%-20s" "$model") | Prompt: $prompt" >> ~/dev/logs/alter-ego/$(date +%y-%m-%d)-prompts.txt
 	curl http://localhost:11434/api/generate -d '{"model": "'$model'",
 		"prompt":"1 line response please: '"$prompt"'",
 		"stream":false
