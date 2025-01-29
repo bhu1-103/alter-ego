5b03b42138383b4a87845abcf9d28b9269dbd672
bhu1-103 <103.bhuvan@gmail.com>
2025-01-30 02:00:42 +0530

    some normal double quotes stuff

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/zsh
 
-model=$(ollama list | tail -n +2 | awk -F " " '{print $1,$3$4}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-7 -p "")
+model=$(ollama list | tail -n +2 | awk -F " " '{print $1}' | rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-7 -p "")
 echo "chosen model is $model" #debug
 
 prompt=$(rofi -dmenu -theme $HOME/.config/rofi/launchers/type-7/style-7 -p "Jarvis with $model here")
 echo "prompt is $prompt" #debug
 if [ -n "$prompt" ]; then
-	curl http://localhost:11434/api/generate -d '{"model": '"$model"',
+	curl http://localhost:11434/api/generate -d '{"model": "'$model'",
 		"prompt":"1 line response please: '"$prompt"'",
 		"stream":false
 		}' > ~/dev/cache/response.json
