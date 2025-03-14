8c385ddba244ca5320091c3baf56bb9bf6f0f171
bhu1-103 <103.bhuvan@gmail.com>
2025-03-14 23:58:48 +0530

    Fri Mar 14 11:58:48 PM IST 2025

---
+++ scripts/clipboard-search-tts.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+curl http://localhost:11434/api/generate -d '{"model": "llama3.2:1b",
+	"prompt":"Tell me about this in 1 line: '"$(wl-paste)"'",
+	"stream":false
+	}' > ~/dev/bhu3/response.json
+jq ".response" ~/dev/bhu3/response.json > ~/dev/bhu3/response.txt
+espeak-ng "$(cat ~/dev/bhu3/response.txt)"
